6dff1fe7deb98bc4fb91d6ebdf129ea2e4c38ee7
Abu Hanif <ahanif2500@gmail.com>
2026-03-31 00:10:57 +0600

    Add URL routing and views for home, about, and mission pages

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'core/home.html'
+    
+class AboutView(TemplateView):
+    template_name = 'core/about.html'
+    
+class MissionView(TemplateView):
+    template_name = 'core/mission.html'
