e1996f8c4ccff4b9e310a638ee8b7488b03a3466
Abu Hanif <ahanif2500@gmail.com>
2026-03-31 00:31:10 +0600

    Update base template to use Django URL template tags for navigation links and enhance dark mode support

--- personal_command_center/settings.py
+++ personal_command_center/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wbln(&h#yfdrqhhq@c)37d8p+3zcfxf40=tb1kjqmxlebvi+h%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver']
 
 
 # Application definition
